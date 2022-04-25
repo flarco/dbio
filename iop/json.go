2b6eabea23031eba02ec20e12430020e9b6c4f5f
Fritz Larco <flarco@live.com>
2022-04-24 23:06:41 -0300

    add JSON extractNestedArray [skip ci]

--- iop/json.go
+++ iop/json.go
@@ -2,7 +2,9 @@ package iop
 
 import (
 	"io"
+	"reflect"
 	"sort"
+	"strings"
 
 	"github.com/flarco/g"
 	"github.com/nqd/flat"
@@ -62,14 +64,20 @@ func (js *jsonStream) nextFunc(it *Iterator) bool {
 	switch payloadV := payload.(type) {
 	case map[string]interface{}:
 		// is one record
-		recordsInterf = []map[string]interface{}{payloadV}
+		recordsInterf = js.extractNestedArray(payloadV)
+		if len(recordsInterf) == 0 {
+			recordsInterf = []map[string]interface{}{payloadV}
+		}
 	case map[interface{}]interface{}:
 		// is one record
 		interf := map[string]interface{}{}
 		for k, v := range payloadV {
 			interf[cast.ToString(k)] = v
 		}
-		recordsInterf = []map[string]interface{}{interf}
+		recordsInterf = js.extractNestedArray(interf)
+		if len(recordsInterf) == 0 {
+			recordsInterf = []map[string]interface{}{interf}
+		}
 	case []interface{}:
 		recordsInterf = []map[string]interface{}{}
 		recList := payloadV
@@ -96,7 +104,6 @@ func (js *jsonStream) nextFunc(it *Iterator) bool {
 				recordsInterf = append(recordsInterf, map[string]interface{}{"data": val})
 			}
 		}
-
 	case []map[interface{}]interface{}:
 		recordsInterf = []map[string]interface{}{}
 		for _, rec := range payloadV {
@@ -160,4 +167,75 @@ func (js *jsonStream) parseRecords(records []map[string]interface{}) {
 		js.buffer <- row
 	}
 	// g.Debug("JSON Stream -> Parsed %d records", len(records))
+}
+
+func (js *jsonStream) extractNestedArray(rec map[string]interface{}) (recordsInterf []map[string]interface{}) {
+	recordsInterf = []map[string]interface{}{}
+	sliceKeyValLen := map[string]int{}
+	maxLen := 0
+
+	for k, v := range rec {
+		value := reflect.ValueOf(v)
+		if value.Kind() == reflect.Slice || value.Kind() == reflect.Array {
+			sliceKeyValLen[k] = value.Len()
+			if value.Len() > maxLen {
+				maxLen = value.Len()
+			}
+		}
+	}
+
+	keys := lo.Filter(lo.Keys(sliceKeyValLen), func(k string, i int) bool {
+		return sliceKeyValLen[k] == maxLen
+	})
+
+	var payload interface{}
+	for _, key := range keys {
+		// have predefined list for now
+		switch strings.ToLower(key) {
+		case "data", "records", "rows", "result":
+			payload = rec[key]
+		}
+	}
+
+	switch payloadV := payload.(type) {
+	case []interface{}:
+		recordsInterf = []map[string]interface{}{}
+		recList := payloadV
+		if len(recList) == 0 {
+			return
+		}
+
+		switch recList[0].(type) {
+		case map[interface{}]interface{}:
+			for _, rec := range recList {
+				newRec := map[string]interface{}{}
+				for k, v := range rec.(map[interface{}]interface{}) {
+					newRec[cast.ToString(k)] = v
+				}
+				recordsInterf = append(recordsInterf, newRec)
+			}
+		case map[string]interface{}:
+			for _, val := range recList {
+				recordsInterf = append(recordsInterf, val.(map[string]interface{}))
+			}
+		default:
+			// is array of single values
+			for _, val := range recList {
+				recordsInterf = append(recordsInterf, map[string]interface{}{"data": val})
+			}
+		}
+	case []map[interface{}]interface{}:
+		recordsInterf = []map[string]interface{}{}
+		for _, rec := range payloadV {
+			newRec := map[string]interface{}{}
+			for k, v := range rec {
+				newRec[cast.ToString(k)] = v
+			}
+			recordsInterf = append(recordsInterf, newRec)
+		}
+	case []map[string]interface{}:
+		recordsInterf = payloadV
+	}
+
+	return recordsInterf
 }
