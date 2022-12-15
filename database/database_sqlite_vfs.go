16a86d1cc707134186ca193e7cc9da4c47aa65ed
Fritz Larco <flarco@live.com>
2022-12-15 06:14:40 -0300

    add built constrained file [skip ci]

---
+++ database/database_sqlite_vfs.go
@@ -0,0 +1,211 @@
+//go:build linux
+// +build linux
+
+package database
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/flarco/dbio"
+	"github.com/flarco/g/net"
+
+	"github.com/flarco/g"
+
+	"github.com/psanford/sqlite3vfs"
+	"github.com/psanford/sqlite3vfshttp"
+)
+
+// SQLiteConn is a Google Big Query connection
+type SQLiteConn struct {
+	BaseConn
+	URL string
+}
+
+// Init initiates the object
+func (conn *SQLiteConn) Init() error {
+
+	conn.BaseConn.URL = conn.URL
+	conn.BaseConn.Type = dbio.TypeDbSQLite
+
+	var instance Connection
+	instance = conn
+	conn.BaseConn.instance = &instance
+
+	// handle S3 url
+	s3URL := conn.GetProp("s3_url")
+	httpURL := conn.GetProp("http_url")
+
+	if s3URL != "" && httpURL == "" {
+		// need to generate pre-signed URL
+		httpURL, err := generateS3PreSignedURL(s3URL, conn.GetProp("aws_access_key_id"), conn.GetProp("aws_secret_access_key"))
+		if err != nil {
+			return g.Error(err, "could not create Pre-Signed HTTP URL for s3 file")
+		}
+		conn.SetProp("http_url", httpURL)
+
+		vfs := sqlite3vfshttp.HttpVFS{
+			URL: httpURL,
+			RoundTripper: &roundTripper{
+				referer:   os.Getenv("DBIO_APP"),
+				userAgent: os.Getenv("DBIO_APP"),
+			},
+		}
+
+		err = sqlite3vfs.RegisterVFS("httpvfs", &vfs)
+		if err != nil {
+			return g.Error(err, "register vfs err")
+		}
+	}
+
+	return conn.BaseConn.Init()
+}
+
+// GetURL returns the processed URL
+func (conn *SQLiteConn) GetURL(newURL ...string) string {
+	connURL := conn.BaseConn.URL
+	if len(newURL) > 0 {
+		connURL = newURL[0]
+	}
+
+	// fix scheme
+	URL := strings.ReplaceAll(
+		connURL,
+		"sqlite://",
+		"file:",
+	)
+
+	if httpURL := conn.GetProp("http_url"); httpURL != "" {
+		URL = URL + "?vfs=httpvfs&mode=ro"
+	}
+
+	return URL
+}
+
+// GenerateUpsertSQL generates the upsert SQL
+func (conn *SQLiteConn) GenerateUpsertSQL(srcTable string, tgtTable string, pkFields []string) (sql string, err error) {
+
+	upsertMap, err := conn.BaseConn.GenerateUpsertExpressions(srcTable, tgtTable, pkFields)
+	if err != nil {
+		err = g.Error(err, "could not generate upsert variables")
+		return
+	}
+
+	_, indexTable := SplitTableFullName(tgtTable)
+
+	indexSQL := g.R(
+		conn.GetTemplateValue("core.create_unique_index"),
+		"index", strings.Join(pkFields, "_")+"_idx",
+		"table", indexTable,
+		"cols", strings.Join(pkFields, ", "),
+	)
+
+	_, err = conn.Exec(indexSQL)
+	if err != nil {
+		err = g.Error(err, "could not create unique index")
+		return
+	}
+
+	sqlTempl := `
+	INSERT INTO {tgt_table} as tgt
+		({insert_fields}) 
+	SELECT {src_fields}
+	FROM {src_table} as src
+	WHERE true
+	ON CONFLICT ({pk_fields})
+	DO UPDATE 
+	SET {set_fields}
+	`
+
+	sql = g.R(
+		sqlTempl,
+		"src_table", srcTable,
+		"tgt_table", tgtTable,
+		"src_tgt_pk_equal", upsertMap["src_tgt_pk_equal"],
+		"src_upd_pk_equal", strings.ReplaceAll(upsertMap["src_tgt_pk_equal"], "tgt.", "upd."),
+		"src_fields", upsertMap["src_fields"],
+		"pk_fields", upsertMap["pk_fields"],
+		"set_fields", strings.ReplaceAll(upsertMap["set_fields"], "src.", "excluded."),
+		"insert_fields", upsertMap["insert_fields"],
+	)
+
+	return
+}
+
+type roundTripper struct {
+	referer   string
+	userAgent string
+}
+
+func (rt *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	if rt.referer != "" {
+		req.Header.Set("Referer", rt.referer)
+	}
+
+	if rt.userAgent != "" {
+		req.Header.Set("User-Agent", rt.userAgent)
+	}
+
+	tr := http.DefaultTransport
+
+	if req.URL.Scheme == "file" {
+		path := req.URL.Path
+		root := filepath.Dir(path)
+		base := filepath.Base(path)
+		tr = http.NewFileTransport(http.Dir(root))
+		req.URL.Path = base
+	}
+
+	return tr.RoundTrip(req)
+}
+
+func generateS3PreSignedURL(s3URL, accessKey, secretKey string) (httpURL string, err error) {
+	if accessKey == "" || secretKey == "" {
+		return "", g.Error("Must provide AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY")
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(
+			accessKey,
+			secretKey,
+			"",
+		),
+		Region:                         aws.String("us-east-1"),
+		S3ForcePathStyle:               aws.Bool(true),
+		DisableRestProtocolURICleaning: aws.Bool(true),
+		Endpoint:                       aws.String(""),
+		// LogLevel: aws.LogLevel(aws.LogDebugWithHTTPBody),
+	})
+	if err != nil {
+		err = g.Error(err, "Could not create AWS session")
+		return
+	}
+
+	s3U, err := net.NewURL(s3URL)
+	if err != nil {
+		err = g.Error(err, "Could not parse s3 url")
+		return
+	}
+
+	svc := s3.New(sess)
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(s3U.Hostname()),
+		Key:    aws.String(strings.TrimPrefix(s3U.Path(), "/")),
+	})
+
+	httpURL, err = req.Presign(time.Minute)
+	if err != nil {
+		err = g.Error(err, "Could not request pre-signed s3 url")
+		return
+	}
+
+	return
+}
