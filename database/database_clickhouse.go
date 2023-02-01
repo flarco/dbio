cd3bbbc6d921e90020ec58e70e8c1b22f098645c
Fritz Larco <flarco@live.com>
2023-02-01 23:52:23 +0000

    bump version: BREAKING CHANGE [no test]

--- database/database_clickhouse.go
+++ database/database_clickhouse.go
@@ -106,6 +106,7 @@ func (conn *ClickhouseConn) BulkImportStream(tableFName string, ds *iop.Datastre
 			// if err != nil {
 			// 	return count, g.Error(err, "could not shape batch stream")
 			// }
+
 		}
 
 		err = func() error {
