963e4e2f083b553e9062d99800d888de9aa97659
Fritz Larco <flarco@live.com>
2023-09-25 19:43:13 -0300

    clean up transforms [nt]

--- iop/transforms.go
+++ iop/transforms.go
@@ -1,16 +1,11 @@
 package iop
 
 import (
-	"strings"
-
 	"github.com/flarco/g"
 	"golang.org/x/text/transform"
 )
 
-var Transforms = map[string]TransformFunc{
-	"replace_accents": func(sp *StreamProcessor, val string) (string, error) { return ReplaceAccents(sp, val) },
-	"trim_space":      func(sp *StreamProcessor, val string) (string, error) { return strings.TrimSpace(val), nil },
-}
+var Transforms = map[string]TransformFunc{}
 
 func ReplaceAccents(sp *StreamProcessor, val string) (string, error) {
 	newVal, _, err := transform.String(sp.accentTransformer, val)
